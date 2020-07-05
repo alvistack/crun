2d69e5b2ba7b83b3574ee5c54820210325174149
Wong Hoi Sing Edison <hswong3i@gmail.com>
2020-07-05 10:57:40 +0800

    nix build -f nix/ --arg disableSystemd true

--- nix/default.nix
+++ nix/default.nix
@@ -1,11 +1,23 @@
-{ system ? builtins.currentSystem }:
+{ system ? builtins.currentSystem, disableSystemd ? false }:
 let
   pkgs = (import ./nixpkgs.nix {
     config = {
       packageOverrides = pkg: {
         criu = (static pkg.criu);
+        gpgme = (static pkg.gpgme);
+        libassuan = (static pkg.libassuan);
+        libgpgerror = (static pkg.libgpgerror);
         libseccomp = (static pkg.libseccomp);
         protobufc = (static pkg.protobufc);
+        glib = (static pkg.glib).overrideAttrs(x: {
+          outputs = [ "bin" "out" "dev" ];
+          mesonFlags = [
+            "-Ddefault_library=static"
+            "-Ddevbindir=${placeholder ''dev''}/bin"
+            "-Dgtk_doc=false"
+            "-Dnls=disabled"
+          ];
+        });
         libcap = (static pkg.libcap).overrideAttrs(x: {
           postInstall = ''
             mkdir -p "$doc/share/doc/${x.pname}-${x.version}"
@@ -46,16 +58,27 @@ let
   self = with pkgs; stdenv.mkDerivation rec {
     name = "crun";
     src = ./..;
+    vendorSha256 = null;
     doCheck = false;
     enableParallelBuilding = true;
-    nativeBuildInputs = [ autoreconfHook git go-md2man pkg-config python3 ];
-    buildInputs = [ criu glibc glibc.static libcap libseccomp protobufc systemd yajl ];
+    outputs = [ "out" ];
+    nativeBuildInputs = [ autoreconfHook bash git pkg-config python3 which ];
+    buildInputs = [ glibc glibc.static criu libcap libseccomp protobufc systemd yajl ];
+    configureFlags = [ "--enable-static" ]
+      ++ lib.optional disableSystemd [ "--disable-systemd" ];
     prePatch = ''
       export CFLAGS='-static'
       export LDFLAGS='-s -w -static-libgcc -static'
       export EXTRA_LDFLAGS='-s -w -linkmode external -extldflags "-static -lm"'
       export CRUN_LDFLAGS='-all-static'
       export LIBS='${criu}/lib/libcriu.a ${glibc.static}/lib/libc.a ${glibc.static}/lib/libpthread.a ${glibc.static}/lib/librt.a ${libcap.lib}/lib/libcap.a ${libseccomp.lib}/lib/libseccomp.a ${protobufc}/lib/libprotobuf-c.a ${protobuf}/lib/libprotobuf.a ${systemd.lib}/lib/libsystemd.a ${yajl}/lib/libyajl_s.a'
+    '';
+    buildPhase = ''
+      patchShebangs .
+      make
+    '';
+    installPhase = ''
+      install -Dm755 crun $out/bin/crun
     '';
   };
 in self
